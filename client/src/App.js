c1f20aa4efb37f0b7c62218fbef343af0bc9b61e
Robert Williams <robert@a3marketing.com>
2020-06-30 20:41:35 -0500

    created basic biocard and imported to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,11 @@
 import React from "react";
 import "./App.css";
+import Biocard from "./components/Biocard";
 
 function App() {
   return (
     <div className="App">
-      <div className="App-header">
-        <h2>Welcome to React</h2>
-      </div>
-      <p className="App-intro">
-        To get started, edit <code>src/App.js</code> and save to reload.
-      </p>
+      <Biocard/>
     </div>
   );
 }
--- client/src/components/Biocard.js
+++ client/src/components/Biocard.js
@@ -1,7 +1,27 @@
 import React from "react";
 
 function Biocard (props) {
-    return 
+    return (
+     
+     <div className="ui card">
+    <div className="content">
+    <p className="header">Kristy</p>
+      <div className="meta">
+      </div>
+      <div className="description">
+        Kristy is an art director living in New York.
+      </div>
+    </div>
+    <div className="extra content">
+      <p>
+    <i className="user icon"></i>
+        22 Friends
+      </p>
+    </div>
+  </div>
+
+);
+    
 }
 
 export default Biocard;
